1713b692b67060ba255882f5cbddae085302201d
eucarious <142058047+eucarious@users.noreply.github.com>
2024-11-04 13:32:21 +0200

    Revert 3-4 to submitted version
    Revert to the version actually submitted in the course.
    Check for issues post grading.

--- wk1/3-4.cpp
+++ wk1/3-4.cpp
@@ -10,25 +10,19 @@ int main() {
     cout << "Enter a year: ";
     cin >> year;
 
-    // classmate showed me a better way 
-    
-    if (year % 400 == 0) {
-        
-        cout << year << " is a leap year!";
-        
-    }
-
-    if (year % 100 == 0) {
-
-        cout << year << " is not a leap year!";
-
-    }
-        
-
+     // ENDLESS NESTED IF STATEMENTS LET'S GOOOO
     if (year % 4 == 0){
 
-        cout << year << " is a leap year!";
-        
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                cout << year << " is a leap year!";
+            } else {
+                cout << year << " is not a leap year!";
+            }
+        } else {
+            cout << year << " is a leap year!";
+        }
+
     } else {
 
         cout << year << " is not a leap year!";
